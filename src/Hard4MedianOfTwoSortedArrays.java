6b0d32fa6e2ba6eb4c0235f38abecdc2371b989e
AlexanderWang <Alexander.Xin.Wang@outlook.com>
2020-11-17 17:41:36 +0800

    Add 2 medium + 1 hard

---
+++ src/Hard4MedianOfTwoSortedArrays.java
@@ -0,0 +1,19 @@
+/**
+ * @ClassName Hard4MedianOfTwoSortedArrays
+ * @Description TODO
+ * @Author xinwang41
+ * @Date 2020/11/17 17:38
+ * @Version 1.0
+ **/
+public class Hard4MedianOfTwoSortedArrays {
+    /**
+     * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+     * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+     */
+    /**
+     * Example 1:                           Example 2:
+     * nums1 = [1, 3]                       nums1 = [1, 2]
+     * nums2 = [2]                          nums2 = [3, 4]
+     * The median is 2.0                    The median is (2 + 3)/2 = 2.5
+     */
+}
---
+++ src/Medium3LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,17 @@
+/**
+ * @ClassName Medium3LongestSubstringWithoutRepeatingCharacters
+ * @Description TODO
+ * @Author xinwang41
+ * @Date 2020/11/17 17:35
+ * @Version 1.0
+ **/
+public class Medium3LongestSubstringWithoutRepeatingCharacters {
+    /**
+     * Given a string, find the length of the longest substring without repeating characters.
+     * Examples:
+     * Given "abcabcbb", the answer is "abc", which the length is 3.
+     * Given "bbbbb", the answer is "b", with the length of 1.
+     * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is
+     * a subsequence and not a substring.
+     */
+}
